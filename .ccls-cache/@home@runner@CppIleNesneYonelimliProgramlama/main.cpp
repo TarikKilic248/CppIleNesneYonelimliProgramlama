3de18bb3db1717474658efe90e1db87c98335550
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-10-14 15:55:49 +0000

    İsim Uzayları ( namespace )

--- .ccls-cache/@home@runner@CppIleNesneYonelimliProgramlama/main.cpp
+++ .ccls-cache/@home@runner@CppIleNesneYonelimliProgramlama/main.cpp
@@ -2,32 +2,24 @@
 
 using namespace std;
 
-class nokta {
-public:
-  int x, y;
-  void degerAta(int x, int y) {
-    this->x = x;
-    this->y = y;
-  }
-  void ekranaYaz() { cout << x << ", " << y << endl; }
-  bool baslangictaMi() { return x == 0 && y == 0; }
-};
+namespace programci {
+int x = 15;
+void Food() {
+  int x = 5;
+  cout << x << endl;
+  ;
+}
+} // namespace programci
+
+int x = 3;
+
+void Boo() { cout << x << endl; }
 
 int main() {
-  nokta n;
-  nokta n2;
-  nokta *n3;
-  n3 = &n2;
-  n.degerAta(2, 7);
-  n.ekranaYaz();
-
-  n2.degerAta(0, 0);
-  if (n2.baslangictaMi()) {
-    cout << "n2 baslangic noktasında" << endl;
-  }
-
-  n2.degerAta(1, 0);
-
-  cout << "n3 pointer ile yazılan değerler" << endl;
-  n3->ekranaYaz();
+  cout << x<<endl;
+  int x = 10;
+  Boo();
+  cout << programci::x << endl;
+  programci::Food();
+  return 0;
 }
