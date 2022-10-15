3390a34acc9f4fd00ababfbb5dc32409564942a3
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-10-15 11:20:31 +0000

    Friend Fonksiyon ve Friend Sınıflar

--- main.cpp
+++ main.cpp
@@ -2,31 +2,23 @@
 
 using namespace std;
 
-class nokta {
+class A {
 private:
   int x;
-  int y;
 
 public:
   int getX() { return x; };
-  int getY() { return y; };
-
   void setX(int x) { this->x = x; };
-  void setY(int y) { this->y = y; };
-
-  void ekranaYaz();
-  bool baslangicdaMi();
+  friend void Foo(A &);
 };
 
-void nokta::ekranaYaz() { cout << x << ", " << y << endl; }
-
-bool nokta::baslangicdaMi() { return x == 0 && y == 0; }
+void Foo(A &a) { a.x = 0; }
 
 int main() {
-  nokta n1;
-  n1.y = 10;
-  n1.degerAta(5, 7);
-  n1.ekranaYaz();
-
+  A n1;
+  n1.setX(5);
+  cout << n1.getX() << endl;
+  Foo(n1);
+  cout << n1.getX() << endl;
   return 0;
 }
