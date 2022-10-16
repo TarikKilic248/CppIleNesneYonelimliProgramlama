9d3cf118e13c509985f81eb76d9e5f2bc1af3297
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-10-16 10:06:05 +0000

    Kurucu Methodlar Pointer ve Dizilerde Nasıl Çalışır

--- main.cpp
+++ main.cpp
@@ -2,23 +2,37 @@
 
 using namespace std;
 
-class A {
+class Nokta {
 private:
   int x;
+  int y;
 
 public:
-  int getX() { return x; };
+  Nokta() {
+    cout << "Parametresiz" << endl;
+    x = 0;
+    y = 0;
+  };
+  Nokta(int x, int y = 0) {
+    cout << "Parametreli" << endl;
+    this->x = x;
+    this->y = y;
+  }
+  int getX() { return x; }
+  int getY() { return y; }
   void setX(int x) { this->x = x; };
-  friend void Foo(A &);
+  void setY(int y) { this->y = y; };
+  void ekranaYaz() { cout << x << ", " << y << endl; }
+  bool baslangictaMi() { return x == 0 && y == 0; };
 };
 
-void Foo(A &a) { a.x = 0; }
-
 int main() {
-  A n1;
-  n1.setX(5);
-  cout << n1.getX() << endl;
-  Foo(n1);
-  cout << n1.getX() << endl;
+  Nokta dizi[5] = {{1, 2}, {1}, {2, 3}};
+
+  for (int i = 0; i < 5; i++) {
+    cout << dizi[i].getX() << ", " << dizi[i].getY() << endl;
+  }
+
+
   return 0;
 }
